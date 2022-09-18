8318e320dea5b3982290875630ba005c30d8ce1a
Robert <commandbee@gmail.com>
2022-09-18 17:50:23 +0100

    Implimenting Javascript

---
+++ js/main.js
@@ -0,0 +1,5 @@
+//Adds Global Footer
+console.log("Add Footer");
+const newFooter = document.createElement("footer");
+newFooter.innerText = "|| Developed By CommandBee • Source Code On Github • Commandbee@gmail.com ||";
+document.body.append(newFooter);
